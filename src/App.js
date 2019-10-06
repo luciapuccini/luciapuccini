7cdea89fe114eda9d8ff1c72e6c1978c49a3c06d
Lucia <puccinilucia@gmail.com>
2019-10-06 19:27:31 -0300

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
+import Container from 'react-bootstrap/Container'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+     <Container >
+       <div style={{marginTop:'20px'}}></div>
+        <h1>Lucia Puccini</h1>
+        <Alert variant='info' style ={{width: '300px', textAlign: 'center'}} >
+            Under Development
+         </Alert>
+        <Button
+          variant="dark"
+          href='https://github.com/luciapuccini/'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        Get To know me!
+      </Button>
+
+     </Container>
   );
 }
 

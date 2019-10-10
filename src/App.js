ed8684b67ab6d7d2fc2c740943828d6608aa9dce
luciapuccini <luciap@nimble.la>
2019-10-09 23:52:23 -0300

    Add profile photo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
-
+import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const profilePhoto = require('./assets/profile-photo.png')
 function App() {
   return (
      <Container >
        <div style={{marginTop:'20px'}}></div>
+        <Row style={{ justifyContent:'center', alignItems:'center'}}>
         <h1>Lucia Puccini</h1>
-        <Alert variant='info' style ={{width: '300px', textAlign: 'center'}} >
+        <Image src={profilePhoto} style={{marginLeft: '20px',height:'100px', width:'100px'}} roundedCircle thumbnail />
+        </Row>
+      
+       <Alert variant='info' style ={{width: '300px', textAlign: 'center'}} >
             Under Development
          </Alert>
         <Button
@@ -18,6 +26,7 @@ function App() {
         >
         Get To know me!
       </Button>
+       
 
      </Container>
   );

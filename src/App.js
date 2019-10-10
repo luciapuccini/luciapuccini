298e4ae8501c24ee75f737d0eafc8916e510e9d7
luciapuccini <luciap@nimble.la>
2019-10-10 00:07:18 -0300

    Add linter setup

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
 import Container from 'react-bootstrap/Container'
+;
 function App() {
-  return (
-     <Container >
-       <div style={{marginTop:'20px'}}></div>
-        <h1>Lucia Puccini</h1>
-        <Alert variant='info' style ={{width: '300px', textAlign: 'center'}} >
-            Under Development
-         </Alert>
-        <Button
-          variant="dark"
-          href='https://github.com/luciapuccini/'
-        >
-        Get To know me!
-      </Button>
-
-     </Container>
-  );
+        return (
+                <Container>
+                        <div style={{ marginTop: '20px' }}></div>
+                        <h1>Lucia Puccini</h1>
+                        <Alert variant="info" style={{ width: '300px', textAlign: 'center' }}>
+                                Under Development
+                        </Alert>
+                        <Button variant="dark" href="https://github.com/luciapuccini/">
+                                Get To know me!
+                        </Button>
+                </Container>
+        );
 }
 
 export default App;

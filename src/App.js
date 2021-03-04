7bb924b1ceff7969ca7c9c878addda8af43eb85c
luciapuccini <luciap@nimble.la>
2021-03-04 18:34:41 -0300

    update tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import logo from "./assets/GitHub_Logo.png";
 
 import Container from "react-bootstrap/Container";
 function App() {
@@ -9,11 +11,21 @@ function App() {
     <Container style={{ marginTop: "100px" }}>
       <h1>Lucia Puccini</h1>
       <Alert variant="info" style={{ width: "300px", textAlign: "center" }}>
-        Under Development
+        Testing page
       </Alert>
-      <Button variant="dark" href="https://github.com/luciapuccini/">
-        Get To know me!
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <a href="zoommtg://updateai/?meetingid=123">update test </a>
+        <a href="updateai://updateai/?meetingid=123">update test </a>
+      </div>
+      <br />
+      <Button href="./assets/sample-zoom-app.zip" download="sample-zoom-app">
+        download app
       </Button>
+      <div style={{ position: "fixed", bottom: 0, right: 0 }}>
+        <a href="https://github.com/luciapuccini/">
+          <Image src={logo} roundedCircle style={{ height: "30px" }} />
+        </a>
+      </div>
     </Container>
   );
 }

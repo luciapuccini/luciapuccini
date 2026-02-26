bd48816b309a7dadcbecb16d464ee230b2e8f2ef
luciapuccini <puccinilucia@gmail.com>
2026-02-26 12:14:21 +0100

    setup vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        
+      </div>
+      <h1>Lucia Puccini</h1>
+      
+    </>
+  )
+}
+
+export default App

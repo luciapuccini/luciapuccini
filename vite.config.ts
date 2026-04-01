bd0cb2a4533e76a0e58b4076e14415bca2f3c743
luciapuccini <puccinilucia@gmail.com>
2026-04-01 10:31:41 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/ollama': {
-        target: 'https://ollama.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/ollama/, ''),
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {},
-    },
-  },
-})
+	plugins: [
+		react(),
+		analyzer({
+			analyzerPort: 8889,
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		proxy: {
+			"/ollama": {
+				target: "https://ollama.com",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/ollama/, ""),
+			},
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {},
+		},
+	},
+});
